14069bbe5d1a2606734fa2d18ef3baa5e232822f
wight-in-tech <chetannb78@gmail.com>
2025-07-16 20:00:09 +0530

    new assignments

--- async_js/index.js
+++ async_js/index.js
@@ -4,3 +4,14 @@ function sum( a , b){
 }
 
 console.log(sum(2,3));
+
+
+function sum1(n){
+    var s=0;
+    for(let i=0;i<=n;i++){
+        s = s +i
+    }
+    return s;
+}
+
+console.log(sum1(5));
